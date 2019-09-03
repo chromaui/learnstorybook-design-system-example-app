14b3d3811f03f5534da2dd0446b67cb0093bc55f
Tom Coleman <tom@thesnail.org>
2019-09-03 14:15:06 +1000

    Use Avatar in UserItem

--- src/components/UserItem.js
+++ src/components/UserItem.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Avatar } from 'learnstorybook-design-system';
 
 const Container = styled.div`
   background: #eee;
   margin-bottom: 1em;
   padding: 0.5em;
-`;
-
-const Avatar = styled.img`
-  border: 1px solid black;
-  width: 30px;
-  height: 30px;
-  margin-right: 0.5em;
 `;
 
 const Name = styled.span`
