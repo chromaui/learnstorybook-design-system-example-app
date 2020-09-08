dbd39323a249b789768c41a50662744d26c9fe4f
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2020-09-08 14:12:30 +0100

    Move packages and stories to 6.0

--- src/components/UserItem.stories.js
+++ src/components/UserItem.stories.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import UserItem from './UserItem';
+import UserItem from "./UserItem";
 
 export default {
   component: UserItem,
-  title: 'components/UserItem',
+  title: "components/UserItem",
 };
 
-export const simple = () => (
-  <UserItem
-    user={{ name: 'Dominic Nyugen', avatarUrl: 'https://avatars2.githubusercontent.com/u/263385' }}
-  ></UserItem>
-);
+// to copy to pull request
+const Template = (args) => <UserItem {...args} />;
+
+export const simple = Template.bind({});
+simple.args = {
+  user: {
+    name: "Dominic Nyugen",
+    avatarUrl: "https://avatars2.githubusercontent.com/u/263385",
+  },
+};
